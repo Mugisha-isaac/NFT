9ea2e59d74cfc89406d85bf9484a74f000d9e042
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-11 11:35:43 +0200

    done with nav

--- app/root.tsx
+++ app/root.tsx
@@ -49,29 +49,32 @@ function Document({children, title}){
 function Layout(){
     return(
       <>
-       <div className="">
+       <div className="px-10 sm:text-sm">
          <div className="m-5">
-            <nav className="grid grid-cols-2 grid-rows-1 gap-3">
-              <div className=" flex flex-row clear-left mt-5 relative">
+            <nav className="grid grid-cols-2 grid-rows-1 gap-3  relative mt-6" style={{width:'85%', marginLeft:'5em'}}>
+              <div className=" flex flex-row clear-left mt-5">
               <div className="w-2 h-2 rounded-full bg-red-400"></div>
              <Link to='/'>Cuans</Link>
               </div>
-              <div className="absolute top-10 px-14 ml-10 px-10" >
-              <ul className="grid grid-rows-1 grid-cols-5 text-center">
+              <div className="absolute top-5 px-14 ml-10 px-10" >
+              <ul className="grid grid-rows-1 grid-cols-6 text-center">
                <li>
-                 <Link to='/about' className="capitalize">About us</Link>
+                 <Link to='/about' className="capitalize p-3">About us</Link>
                </li>
                <li>
-                 <Link to='/benefits' className="capitalize">The Benefits</Link>
+                 <Link to='/benefits' className="capitalize p-3">The Benefits</Link>
                </li>
                <li>
-                 <Link to='/how' className="capitalize">How It Works </Link>
+                 <Link to='/how' className="capitalize p-3">How It Works </Link>
                </li>
                <li>
-                 <Link to='/premium' className="capitalize">Premium Plans </Link>
+                 <Link to='/premium' className="capitalize p-3">Premium Plans </Link>
                </li>
                <li>
-                 <Link to='/contact' className="capitalize">Contact Us </Link>
+                 <Link to='/contact' className="capitalize p-3">Contact Us </Link>
+               </li>
+               <li>
+               <Link to='auth/register' className="capitalize p-3 text-bold ml-3 bg-black text-white rounded-full">create account</Link>
                </li>
              </ul>
               </div>
